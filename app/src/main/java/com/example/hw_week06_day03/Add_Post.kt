44e30483630d0e17a3e9a0f1bf5f32eeb2250282
Amar <suliseg@hotmail.com>
2021-11-16 22:09:49 +0300

    HW_Week06_Day03

---
+++ app/src/main/java/com/example/hw_week06_day03/Add_Post.kt
@@ -0,0 +1,23 @@
+package com.example.hw_week06_day03
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+
+class Add_Post : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_post)
+        val postImageView = findViewById<ImageView>(R.id.imageViewImageLink)
+        val imageEditText = findViewById<EditText>(R.id.editTextImageLink)
+        val nameEditText = findViewById<EditText>(R.id.editTextName)
+        val contentEditText =findViewById<EditText>(R.id.editTextContent)
+
+
+            }
+        }
+
+
